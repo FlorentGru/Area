ddb4bf196bf5c6fff924f0b80a443a84b4ad0e64
Florent Gru <florent.gru@epitech.eu>
2020-02-20 15:26:04 +0100

    Create the login Page and the register Page, now need to call the API for create user and can check for login if they already exist

--- Mobile/app/src/main/java/com/example/area/MainActivity.kt
+++ Mobile/app/src/main/java/com/example/area/MainActivity.kt
@@ -1,12 +1,67 @@
 package com.example.area
 
+import android.animation.Animator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.view.View.VISIBLE
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signUp.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Registering", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginButton.setOnClickListener {
+            
+        }
+
+        object : CountDownTimer(2000, 1000) {
+            override fun onFinish() {
+                bookITextView.visibility = View.GONE
+                loadingProgressBar.visibility = View.GONE
+                rootView.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.colorSplashText))
+                bookIconImageView.setImageResource(R.drawable.background_color_book)
+                startAnimation()
+            }
+            override fun onTick(p0: Long) {}
+        }.start()
+    }
+
+    private fun startAnimation() {
+        bookIconImageView.animate().apply {
+            x(50f)
+            y(100f)
+            duration = 1000
+        }.setListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                afterAnimationView.visibility = VISIBLE
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+
+            }
+        })
     }
 }
