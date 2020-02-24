9adb8dc8aec5e110a8689ad3bd9d2edcc3eac35d
Florent Gru <florent.gru@epitech.eu>
2020-02-24 13:41:03 +0100

    Try to add the part who we call the api for login and registration

--- Mobile/app/src/main/java/com/example/area/HttpRequest.kt
+++ Mobile/app/src/main/java/com/example/area/HttpRequest.kt
@@ -1,10 +1,19 @@
 package com.example.area
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
+
+
+
 fun sendPostRequest(userName:String, password:String) {
 
     var reqParam = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(userName, "UTF-8")
     reqParam += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8")
-    val mURL = URL("<Your API Link>")
+    val mURL = URL("localhost:8080")
 
     with(mURL.openConnection() as HttpURLConnection) {
         // optional default is GET
@@ -31,3 +40,30 @@ fun sendPostRequest(userName:String, password:String) {
     }
 }
 
+fun sendGetRequest(userName:String, password:String) {
+
+    var reqParam = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(userName, "UTF-8")
+    reqParam += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8")
+
+    val mURL = URL("localhost:8080"+reqParam)
+
+    with(mURL.openConnection() as HttpURLConnection) {
+        // optional default is GET
+        requestMethod = "GET"
+
+        println("URL : $url")
+        println("Response Code : $responseCode")
+
+        BufferedReader(InputStreamReader(inputStream)).use {
+            val response = StringBuffer()
+
+            var inputLine = it.readLine()
+            while (inputLine != null) {
+                response.append(inputLine)
+                inputLine = it.readLine()
+            }
+            it.close()
+            println("Response : $response")
+        }
+    }
+}
