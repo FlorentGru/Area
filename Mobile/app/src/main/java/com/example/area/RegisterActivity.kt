df9f17c1649989aabd017e86617d87be7a956383
Florent Gru <florent.gru@epitech.eu>
2020-02-28 01:35:06 +0100

    Add the connection with our server, the user can almost register and login he just need to enter the ngrok tunnel

--- Mobile/app/src/main/java/com/example/area/RegisterActivity.kt
+++ Mobile/app/src/main/java/com/example/area/RegisterActivity.kt
@@ -5,14 +5,49 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_register.*
+import org.json.JSONObject
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val ipAddress = findViewById<EditText>(R.id.ipText)
+        val ipString = ipAddress.text.toString()
+        val register = "/auth/register"
+        val url = "$ipString $register"
+
+        val jsonobj = JSONObject()
+
+        registerBtn.setOnClickListener {
+            jsonobj.put("email",etadress.text)
+            jsonobj.put("name",etname.text)
+            jsonobj.put("password",etpassword.text)
+            Toast.makeText(this, url, Toast.LENGTH_SHORT).show()
+
+            val que = Volley.newRequestQueue(this)
+            val req = JsonObjectRequest(
+                Request.Method.POST,url,jsonobj,
+                Response.Listener {
+                        response ->
+                    Toast.makeText(this, response["success"].toString(), Toast.LENGTH_SHORT).show()
+
+                }, Response.ErrorListener {
+                    response ->
+                    Toast.makeText(this, response.toString(), Toast.LENGTH_SHORT).show()
+                })
+            que.add(req)
+        }
 
         tvlogin.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
