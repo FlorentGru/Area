a99ae9c5240a1a310bfc99504df26a345fc425ba
Baptiste LE NEDIC <baptiste.le-nedic@epitech.eu>
2020-02-29 23:15:50 +0100

    testing outlook hotmail

--- Server/sources/services/outlookReaction.js
+++ Server/sources/services/outlookReaction.js
@@ -1,21 +1,31 @@
 const nodemailer = require('nodemailer');
 
 exports.sendMail = async function(to, subject, content) {
-    let transporter = await nodemailer.createTransport({
-        host: "smtp-mail.outlook.com",
-        secureConnection: false,
-        port: 587,
-        tls: {
-            ciphers:'SSLv3'
-        },
+    var transport = nodemailer.createTransport("SMTP", {
+        service: "hotmail",
         auth: {
-            user: "area.epitech.nantes.2022@outlook.fr",
-            pass: "Hasherlesmdpcestpourlesnuls",
+            user: "onmeritelegradeA@hotmail.com",
+            pass: "Hasherlesmdpcestpourlesnuls"
         }
     });
+    /*
+        let transporter = await nodemailer.createTransport({
+            host: "smtp-mail.outlook.com",
+            secureConnection: false,
+            port: 587,
+            tls: {
+                ciphers:'SSLv3'
+            },
+            auth: {
+                user: "area.epitech.nantes.2022@outlook.fr",
+                pass: "Hasherlesmdpcestpourlesnuls",
+            }
+        });
+
+     */
     console.log("here");
     let info = await transporter.sendMail({
-        from: '"AREA" <area.epitech.nantes.2022@outlook.fr>',
+        from: '"AREA" <onmeritelegradeA@hotmail.com>',
         to: to,
         subject: subject,
         text: content,
