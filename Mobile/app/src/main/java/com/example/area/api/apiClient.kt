847d5c4921daf6682016222cf907cc43fc837380
Florent Gru <florent.gru@epitech.eu>
2020-02-29 16:37:48 +0100

    Add some part for call the api and connect with the oauth to service

---
+++ Mobile/app/src/main/java/com/example/area/api/apiClient.kt
@@ -0,0 +1,16 @@
+package com.example.area.Api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class apiClient() {
+    companion object Client {
+        fun setRetrofit(base_url: String):Retrofit {
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit;
+        }
+    }
+}
--- Mobile/app/src/main/java/com/example/area/view/connection/ConnectionViewModel.kt
+++ Mobile/app/src/main/java/com/example/area/view/connection/ConnectionViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.area.view.connection
 
-import android.os.Bundle
-import android.view.View
 import androidx.lifecycle.ViewModel
 
 class ConnectionViewModel : ViewModel()
