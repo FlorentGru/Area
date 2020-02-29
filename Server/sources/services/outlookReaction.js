cca99c4e9d69f2060ea0fa525a90de1e0e21b189
Baptiste LE NEDIC <baptiste.le-nedic@epitech.eu>
2020-03-01 00:12:42 +0100

    using zoho instead of outlook (tryout)

--- Server/sources/services/outlookReaction.js
+++ Server/sources/services/outlookReaction.js
@@ -1,17 +1,10 @@
 const nodemailer = require('nodemailer');
 
+/*
 exports.sendMail = async function(to, subject, content) {
-    var transport = nodemailer.createTransport("SMTP", {
-        service: "hotmail",
-        auth: {
-            user: "onmeritelegradeA@hotmail.com",
-            pass: "Hasherlesmdpcestpourlesnuls"
-        }
-    });
-    /*
         let transporter = await nodemailer.createTransport({
             host: "smtp-mail.outlook.com",
-            secureConnection: false,
+            secureConnection: 'SARTTLS',
             port: 587,
             tls: {
                 ciphers:'SSLv3'
@@ -21,8 +14,6 @@ exports.sendMail = async function(to, subject, content) {
                 pass: "Hasherlesmdpcestpourlesnuls",
             }
         });
-
-     */
     console.log("here");
     let info = await transporter.sendMail({
         from: '"AREA" <onmeritelegradeA@hotmail.com>',
@@ -39,3 +30,34 @@ exports.sendMail = async function(to, subject, content) {
     });
     console.log(info);
 };
+
+ */
+
+
+exports.sendMail = async function(to, subject, content) {
+    let transporter = await nodemailer.createTransport({
+        host: 'smtp.zoho.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: "admin@bonacte.com",
+            pass: "Hasherlesmdpcestpourlesnuls",
+        }
+    });
+    console.log("here");
+    let info = await transporter.sendMail({
+        from: '"AREA" <admin@bonacte.com>',
+        to: to,
+        subject: subject,
+        text: content,
+        html: `<p>${content}</p>`
+    }, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Email Sent !");
+        }
+    });
+    console.log(info);
+};
+
