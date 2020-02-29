abaca9858ffdaa85df0c694a960cdf0e4ef1f03f
Florent Gru <florent.gru@epitech.eu>
2020-02-29 04:31:02 +0100

    Repair the navigation bar who now work well with the 3 part, connection, api, services

--- Mobile/app/src/main/java/com/example/area/navBar.kt
+++ Mobile/app/src/main/java/com/example/area/navBar.kt
@@ -3,24 +3,49 @@ package com.example.area
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.area.view.connection.ConnectionFragment
+import com.example.area.view.networkLocation.NetworkFragment
+import com.example.area.view.service.ServiceFragment
+import kotlinx.android.synthetic.main.navbar.*
 
 class navBar : AppCompatActivity()
 {
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item->
+        when(item.itemId){
+            R.id.connection -> {
+                println("Connection pressed")
+                replaceFragment(ConnectionFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.service -> {
+                println("Service pressed")
+                replaceFragment(ServiceFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.notifications -> {
+                println("Ip pressed")
+                replaceFragment(NetworkFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navbar)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_connection, R.id.navigation_service, R.id.navigation_network_location))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container, fragment)
+        fragmentTransaction.commit()
     }
 }
