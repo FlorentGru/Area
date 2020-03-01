208b4450938f27e131f722247314371732250ab8
Baptiste LE NEDIC <baptiste.le-nedic@epitech.eu>
2020-03-01 16:48:31 +0100

    correcting zoho

---
+++ Server/sources/services/zohoReaction.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+exports.sendMail = async function(to, subject, content) {
+    let transporter = await nodemailer.createTransport({
+        host: 'smtp.zoho.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: "area.epitek.nantes.2022@zohomail.com",
+            pass: "Hasherlesmdpcestpourlesnuls#1",S
+        }
+    });
+    console.log("here");
+    let info = await transporter.sendMail({
+        from: '"AREA" <area.epitek.nantes.2022@zohomail.com>',
+        to: to,
+        subject: subject,
+        text: content,
+        html: `<p>${content}</p>`
+    }, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Email Sent !");
+        }
+    });
+    console.log(info);
+};
+
