e651419833563bd09510ab25664bd58a0628bb36
Lucas <lucas.huet-de-guerville@epitech.eu>
2020-03-22 19:06:02 +0100

    add the encryption of password

--- web_app/src/APICalls/APILogin.js
+++ web_app/src/APICalls/APILogin.js
@@ -1,14 +1,26 @@
 const axios = require('axios').default;
+const crypto = require('crypto')
+const algorithm = 'aes-256-ecb'
+const key = new Buffer("aed60beab8122e81536c33ec4614d97bb6367d3cd13cadc4e7ed095026aa30d4", 'hex');
+
+function encrypt(text){
+    let iv = new Buffer('');
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    let crypted = cipher.update(text,'utf8','hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
 
 export default async function loginUser(user_email, user_password) {
     try {
+        const encryptedPassword = encrypt(user_password)
         const response = await axios({
             method: "post",
             url: `${localStorage.getItem("address")}/user/login`,
             headers: {'Content-Type': 'application/json'},
             data: {
                 "email": user_email,
-                "password": user_password
+                "password": encryptedPassword
             }
         })
         return (response)
--- web_app/src/APICalls/APIRegister.js
+++ web_app/src/APICalls/APIRegister.js
@@ -1,7 +1,19 @@
 const axios = require('axios').default;
+const crypto = require('crypto')
+const algorithm = 'aes-256-ecb'
+const key = new Buffer("aed60beab8122e81536c33ec4614d97bb6367d3cd13cadc4e7ed095026aa30d4", 'hex');
+
+function encrypt(text){
+    let iv = new Buffer('');
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    let crypted = cipher.update(text,'utf8','hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
 
 export default async function registerUser(user_name, user_password, user_email) {
     try {
+        const encryptedPassword = encrypt(user_password)
         const response = await axios({
             method: "post",
             url: `${localStorage.getItem("address")}/user/register`,
@@ -9,7 +21,7 @@ export default async function registerUser(user_name, user_password, user_email)
             data: {
                 "email": user_email,
                 "name": user_name,
-                "password": user_password
+                "password": encryptedPassword
             }
         })
         return (response.status)
