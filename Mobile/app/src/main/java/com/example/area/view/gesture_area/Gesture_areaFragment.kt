14b56659f2d023da068bc1bdb5b23bef9e17e7c5
unknown <jean.delaunay@epitech.eu>
2020-03-22 22:33:04 +0100

    Now you can delete area

--- Mobile/app/src/main/java/com/example/area/view/gesture_area/Gesture_areaFragment.kt
+++ Mobile/app/src/main/java/com/example/area/view/gesture_area/Gesture_areaFragment.kt
@@ -3,20 +3,23 @@ package com.example.area.view.gesture_area
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.android.volley.toolbox.JsonObjectRequest
 import com.example.area.R
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.Volley
 import com.example.area.navBar
+import com.google.gson.JsonObject
 import org.json.JSONObject
 import org.json.JSONArray
 
@@ -33,11 +36,10 @@ class Gesture_areaFragment : Fragment() {
     ): View? {
         _view = inflater.inflate(R.layout.gesture_area, container, false)
 
-        val NameArea = TextView(activity)
         val buttonDelete = Button(activity)
         val buttonDetails = Button(activity)
-        val listAction: List<String> = emptyList<String>()
-        var text = _view!!.findViewById<TextView>(R.id.listAction)
+        val singleAction = ArrayList<String>()
+        val listAction = ArrayList<ArrayList<String>>()
 
         val linearLayout = _view!!.findViewById<LinearLayout>(R.id.firstLayout)
         buttonDelete.text = "Delete"
@@ -50,20 +52,37 @@ class Gesture_areaFragment : Fragment() {
         buttonDetails.setTextColor(Color.BLACK)
 
         val que = Volley.newRequestQueue(activity)
-        //Récupération des AREA créer avec un GET sur le serveur
-        //Une fois la récupération faite, affiage des graphs (Le nom de l'area + le boutton détail + boutton delete
         val giveAction = object : JsonObjectRequest(
             Request.Method.GET,
             _baseUrl + "user/areas",
             null,
             Response.Listener{
                 response->
-                val jsonObj : JSONObject = JSONObject(response.toString())
-                val data : JSONArray = JSONArray(jsonObj.getString("data"))
-               //for (i in 0 until data.length()){
-                 //  val serv = data.getJSONObject(i)
-                   //val id = serv.getString("id")
-                //}
+                val jsonObj = JSONObject(response.toString())
+                val data : JSONArray = jsonObj.getJSONArray("data")
+                var i = 0;
+                listAction.clear()
+                while (i < data.length()) {
+                    val serv = data.getJSONObject(i)
+                    val id = serv.get("_id").toString()
+                    val action = serv.getJSONObject("action")
+                    val paramAction = action.getJSONArray("params")
+                    val reaction = serv.getJSONObject("reaction")
+                    val paramReaction = reaction.getJSONArray("params")
+                    singleAction.add(id)
+                    singleAction.add(action.get("service").toString())
+                    singleAction.add(action.get("name").toString())
+                    singleAction.add(paramAction.getJSONObject(i).get("name").toString())
+                    singleAction.add(paramAction.getJSONObject(i).get("value").toString())
+                    singleAction.add(reaction.get("service").toString())
+                    singleAction.add(reaction.get("name").toString())
+                    singleAction.add(paramReaction.getJSONObject(i).get("name").toString())
+                    singleAction.add(paramReaction.getJSONObject(i).get("value").toString())
+                    createTextAndButton(singleAction, linearLayout)
+                    listAction.add(singleAction)
+                    singleAction.clear()
+                    i++
+                }
             }, Response.ErrorListener {
             }) {
             override fun getHeaders(): MutableMap<String, String?> {
@@ -74,5 +93,46 @@ class Gesture_areaFragment : Fragment() {
         }
         que.add(giveAction)
         return _view
+    }
+
+
+    fun createTextAndButton(array : ArrayList<String>, linear : LinearLayout) {
+        val id = array[0]
+        val textView = TextView(activity)
+        textView.layoutParams = RelativeLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        textView.marginTop
+        textView.text = "Id: " + id + "\nService action: " + array[1] + " name: " + array[2] + "\nService reaction: " + array[5] + " name: " + array[6]
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
+        textView.setTextColor(resources.getColor(R.color.whiteFont))
+        linear.addView(textView)
+
+        val button = Button(activity)
+        button.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        button.text = "DELETE"
+        button.setTextColor(Color.BLACK)
+        button.setBackgroundColor(Color.RED)
+        button.setOnClickListener {
+            val que = Volley.newRequestQueue(activity)
+            val giveAction = object : JsonObjectRequest(
+                Request.Method.DELETE,
+                _baseUrl + "user/areas/delete?areaId=" + id,
+                null,
+                Response.Listener{
+                }, Response.ErrorListener {
+                }) {
+                override fun getHeaders(): MutableMap<String, String?> {
+                    val headers = HashMap<String, String?>()
+                    headers["Authorization"] = _token
+                    return headers
+                }
+            }
+            que.add(giveAction)
+            textView.isVisible = false
+            button.isVisible = false
+        }
+        linear.addView(button)
     }
 }
